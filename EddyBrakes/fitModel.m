ded228ecc67fbbb135a551e53991366244780e48
sshersh <sam@shersher.com>
2020-08-16 12:39:26 -0400

    Added model fitting code

---
+++ EddyBrakes/fitModel.m
@@ -0,0 +1,9 @@
+%% Fit Simulation points
+% Model used is Fd = p1*v/(v^2 + q2) where v is the velocity of magnet
+% array
+% Must have column vectors x, data loaded into workspace
+x = x';
+%Set coefficients of unnecessary terms to 0, and make sure q2 is positive
+options = fitoptions('rat12','Lower',[-Inf 0 0 0],'Upper',[Inf 0 0 Inf]);
+f = fit(x, data, 'rat12', options);
+plot(f, x, data);
